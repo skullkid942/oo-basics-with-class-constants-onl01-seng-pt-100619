121ad49c46e697a32bf23f36f4a9fb66fca83689
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-24 17:50:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    
   end
 
   def cobble
@@ -13,9 +14,4 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def brand=(brand)
-    @brand = brand 
-    BRANDS << brand  unless BRANDS.any? { |b| b == brand }
-  end 
-
 end
